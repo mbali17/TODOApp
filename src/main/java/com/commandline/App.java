5076fc064a19491a1a33ce250cf3760a5369a11e
mbali17 <alimirbasheer@gmail.com>
2017-09-18 01:16:56 -0500

    Adding project Structure

---
+++ src/main/java/com/commandline/App.java
@@ -0,0 +1,13 @@
+package com.commandline;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
